42ab65a19ab13c82e7bb04770384bc0ddd619019
TaeObj <114365424+TaeObj@users.noreply.github.com>
2023-03-19 01:19:12 +0200

    final home update

--- Exam-Yummy/js/ingred-listing.js
+++ Exam-Yummy/js/ingred-listing.js
@@ -14,7 +14,7 @@ export class IngredCard {
     ingCard.innerHTML = `
     <a href="#!" class="d-flex flex-column rounded-3 h-100 text-dark ing-card bg-white text-decoration-none" data-title="${this.title}">
       <div class="ing-feature-img position-relative">
-          <img src="../../assets/imgs/ingred-icon.png" class="img-fluid">          
+          <img src="../assets/imgs/ingred-icon.png" class="img-fluid">          
         </div>
       <div class="ing-info p-3 d-flex flex-column flex-grow-1">
           <h2 class="ing-title fs-5 text-danger">${this.title}</h2>               
